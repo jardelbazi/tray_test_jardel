55236a1e0d833bb934efaa6a43e8ea06b5261b21
Jardel Bazi <jardelbazi@gmail.com>
2025-03-26 17:14:40 -0300

    chore: investigated CORS issues, but no success

---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+};
